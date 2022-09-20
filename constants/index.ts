cb6ad626376b835f44cf7a86fc13d85fef027a21
Shrusti Sambhaji Mane <47842830+Shrustim@users.noreply.github.com>
2022-09-20 19:46:41 +0530

    Delete index.ts

--- constants/index.ts
+++
@@ -1,4 +0,0 @@
-export const JWTSecret = "mysoftwareupdates-prototype";
-export const BaseURL = "http://localhost:3000/";
-export const EmailAddress = "shrustimanexyz@gmail.com";
-export const EmailPassword = "delnttatsdkecuaa";
